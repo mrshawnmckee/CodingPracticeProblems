876143f65acd42641b0a8924a1692a371f1c8b1d
Shawn McKee <shawnroosemckee@gmail.com>
2023-09-21 22:28:05 -0400

    recursive challenge 2 and 3

--- recursionPractice.js
+++ recursionPractice.js
@@ -1,3 +1,7 @@
+// This is an article for interview prep on recursion
+// https://www.codingame.com/playgrounds/5422/js-interview-prep-recursion
+
+// Challenge 01:
 // Write a function called sumRange taht takes a number and returns the sum of all numbers from t up to the number passed in
 
 function sumRange(n, total = 0){
@@ -10,3 +14,29 @@ function sumRange(n, total = 0){
 }
 
 console.log(sumRange(3));
+
+// Challenge 02
+// Write a function called power that takes  base and an exponent. if the exponent is zero, return 1
+function power(n, exp){
+    if(exp == 1){
+        return n
+    } else {
+        return n * power(n, exp-1)
+    }
+}
+
+console.log(power(2,2))
+
+// Challenge 03
+// Write a function that returns the factorial of a number
+
+function factorial(n){
+    if(n === 1){
+        return n
+    } else {
+        return n * factorial(n-1)
+    }
+    
+}
+
+console.log(factorial(5))
