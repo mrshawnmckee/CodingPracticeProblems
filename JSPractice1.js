8fbf02add4673d43d8f70ac7973d86265ca1aaf4
Shawn McKee <shawnroosemckee@gmail.com>
2023-09-27 15:19:44 -0400

    some code wars practice problems

--- JSPractice1.js
+++ JSPractice1.js
@@ -70,51 +70,119 @@
 
 
 
-//  Fibonacci Series
+// //  Fibonacci Series
 
-function fibonacci(num){
-    let previousNum = 0;
-    let currentNum = 1;
-    let sum;
-    for(let i = 2; i < num; i++){
-        sum = previousNum + currentNum
-        previousNum = currentNum
-        currentNum = sum
-    }
-    return sum
+// function fibonacci(num){
+//     let previousNum = 0;
+//     let currentNum = 1;
+//     let sum;
+//     for(let i = 2; i < num; i++){
+//         sum = previousNum + currentNum
+//         previousNum = currentNum
+//         currentNum = sum
+//     }
+//     return sum
+// }
+// console.log(fibonacci(8))
+
+// // Fibonacci create an array
+
+// function fibonaccirArray(num){
+//     let newArray = [0,1];
+//     let sum;
+//     let num1 = 0;
+//     let num2 = 1;
+
+//     for(let i = 2; i < num; i++){
+//         sum = num1 + num2
+//         num1 = num2
+//         num2 = sum
+//         newArray.push(num2)
+//     }
+//     return newArray
+// }
+
+// console.log(fibonaccirArray(8));
+
+// //  fibonacci array with recursion
+
+
+// function recursiveFibonacci(n){
+
+//     if(n < 2){
+//         return n
+//     } else {
+//         return (recursiveFibonacci(n-1) + recursiveFibonacci(n -2))
+//     }
+// }
+
+
+// console.log(recursiveFibonacci(5))
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Running some random CodeWars Challenges, attempting some here
+
+
+// //  Write a function which calculates the average of the numbers in a given list.
+// // Note: Empty arrays should return 0.
+// function findAverage(array) {
+//     // your code here, 
+//     let avg = 0;
+//     let sum = 0;
+//     let count = 0;
+    
+//     if (array.length === 0){
+//       return 0;
+      
+//     } else {
+//        for(let i = 0; i < array.length; i++){
+//          sum += array[i]
+//          count ++    
+//        }      
+//     }
+//     return avg = sum / count
+// }
+
+// console.log(findAverage([1,2,3]))
+
+// // Multiply a given number by 8 if it is even, multiply time 9 if it is odd
+
+// function simpleMultiplication(number) {
+//     // your code........
+//     return number % 2 === 0 ? number * 8 : number * 9;
+//     // if(number % 2 === 0){
+//     //     return number * 8
+//     // } else {
+//     //     return number * 9
+//     // }
+// }
+
+// console.log(simpleMultiplication(8))
+
+// Make negative
+
+// function makeNegative(num){
+
+//     return num >= 0 ? num - num - num : num
+//     //     if(num >= 0){
+//     //     return num - num - num
+//     // } else {
+//     //     return num
+//     // }
+// }
+
+// console.log(makeNegative(-30));
+
+function createPhoneNumber (array){
+    let phoneString = array.join('');
+    console.log(phoneString)
+    let firstGroup = phoneString.slice(0,3)
+    let secondGroup = phoneString.slice(3,6)
+    let lastGroup = phoneString.slice(6)
+    let phoneNumber = `"(${firstGroup}) ${secondGroup}-${lastGroup}"`
+    return phoneNumber
 }
-console.log(fibonacci(8))
 
-// Fibonacci create an array
-
-function fibonaccirArray(num){
-    let newArray = [0,1];
-    let sum;
-    let num1 = 0;
-    let num2 = 1;
-
-    for(let i = 2; i < num; i++){
-        sum = num1 + num2
-        num1 = num2
-        num2 = sum
-        newArray.push(num2)
-    }
-    return newArray
-}
-
-console.log(fibonaccirArray(8));
-
-//  fibonacci array with recursion
-
-
-function recursiveFibonacci(n){
-
-    if(n < 2){
-        return n
-    } else {
-        return (recursiveFibonacci(n-1) + recursiveFibonacci(n -2))
-    }
-}
-
-
-console.log(recursiveFibonacci(5))
+console.log(createPhoneNumber([1,2,3,4,5,6,7,8,9,0]))
